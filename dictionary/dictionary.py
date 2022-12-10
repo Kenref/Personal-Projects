5fd815c3b96897f1e6e94ce5638c361836b05847
Kenneth <kenneth.tseee@gmail.com>
2022-12-10 16:28:18 +1100

    Dictionary progress

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -12,6 +12,10 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
+background_image = tk.PhotoImage(file="background1.jpg")
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
+
 #smaller window within window (border)
 display_frame = tk.Frame(root, bg="#18c8ba", bd=5)
 display_frame.place(relx=0.5, rely=0.05, relwidth=0.9, relheight=0.5, anchor="n")
@@ -29,8 +33,38 @@ interactive_frame.place(relx=0.5, rely=0.6, relwidth=0.9, relheight=0.35, anchor
 #drop down box top left remove button top right
 #textbox bottom left remove bottom right remove buttton
 
+#dropdown menu for removing words
+# make dropdown menu in alphabetical order
+list_of_words = [
+"hi", 
+"bye"
+]
+
+selection = tk.StringVar()
+selection.set(list_of_words[0])
+
+dropdown_menu = tk.OptionMenu(interactive_frame, selection, *list_of_words)
+dropdown_menu.place(relx=0.1, rely=0.3, relwidth=0.5, relheight=0.2)
+
+def remove_word():
+    ...
+    # lorem = Label(root, text=clicked.get())
+    # lorem.pack()
+# use this section do add functionality for removing the word
+
+remove_button = tk.Button(interactive_frame, text="Remove", command=remove_word)
+remove_button.place(relx=0.65, rely=0.3, relwidth=0.25, relheight=0.2)
+
+
 text_box = tk.Entry(interactive_frame, font=40)
-text_box.place(relwidth=0.8, relheight=1, anchor="n")
+text_box.place(relx=0.1, rely=0.6, relwidth=0.5, relheight=0.2)
+
+def add_word():
+    ...
+
+add_button = tk.Button(interactive_frame, text="Add", command=add_word)
+add_button.place(relx=0.65, rely=0.6, relwidth=0.25, relheight=0.2)
+
 
 
 
