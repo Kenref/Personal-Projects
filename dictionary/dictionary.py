2164fb170aaf1d9f675e5c7e6e619941ce1c32ee
Kenneth <kenneth.tseee@gmail.com>
2022-12-13 12:29:48 +1100

    Implemented button functionality

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -20,9 +20,11 @@ background_label.place(relwidth=1, relheight=1)
 display_frame = tk.Frame(root, bg="#18c8ba", bd=5)
 display_frame.place(relx=0.5, rely=0.05, relwidth=0.9, relheight=0.5, anchor="n")
 
+#word of the day at the top
 word = tk.Label(display_frame, text=WORD, font="arial 30")
 word.place(relx=0.5, rely=0.02, relwidth=0.97, relheight=0.3, anchor="n")
 
+#word definition below wotd
 definition = tk.Message(display_frame, text=TEXT, font="arial 14") 
 definition.place(relx=0.5, rely=0.35, relwidth=0.97, relheight=0.63, anchor="n")
 
@@ -30,25 +32,32 @@ definition.place(relx=0.5, rely=0.35, relwidth=0.97, relheight=0.63, anchor="n")
 interactive_frame = tk.Frame(root, bg="#18c8ba")
 interactive_frame.place(relx=0.5, rely=0.6, relwidth=0.9, relheight=0.3, anchor="n")
 
+#remove word button functionality
 def remove_word():
-    with open("words.txt", "a") as file:
-        # to do tomorrow
+    with open("words.txt", "r") as file:
+        inpt = text_box.get()
+        lines=file.readlines()
+    with open("words.txt", "w") as file:
+        for line in lines:
+            if line.strip("\n") != inpt:
+                file.write(line)
 
+#remove word button
 remove_button = tk.Button(interactive_frame, text="Remove", command=remove_word)
 remove_button.place(relx=0.67, rely=0.1, relwidth=0.3, relheight=0.4)
 
-
+#text box
 text_box = tk.Entry(interactive_frame, font="arial 20")
 text_box.place(relx=0.03, rely=0.1, relwidth=0.6, relheight=0.8)
 
+#add word functionality
 def add_word():
     with open("words.txt", "a") as file:
         inpt = text_box.get()
         if inpt.isalpha():
             file.write(f"{inpt.lower()}\n")
 
-
-
+#add word button
 add_button = tk.Button(interactive_frame, text="Add", command=add_word)
 add_button.place(relx=0.67, rely=0.5, relwidth=0.3, relheight=0.4)
 
@@ -60,7 +69,7 @@ add_button.place(relx=0.67, rely=0.5, relwidth=0.3, relheight=0.4)
 
 
 
-
+#everyday is a random with time
 
 #find a way to check whether word is legit in the api to verify whether to add it in the words list or not
 
